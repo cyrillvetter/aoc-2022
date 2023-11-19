7702436768e076a86396b71d05eac1db59eb3d89
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-11-19 19:11:01 +0100

    minor improvement

--- haskell/Day01.hs
+++ haskell/Day01.hs
@@ -4,7 +4,7 @@ import Data.List (sort)
 main = do
     calories <- sumCalories <$> readFile "inputs/1.txt"
     print $ maximum calories
-    print $ sum . take 3 . reverse . sort $ calories
+    print $ sum $ take 3 $ reverse $ sort $ calories
 
 sumCalories :: String -> [Int]
 sumCalories = map (sum . map read . lines) . splitOn "\n\n"
