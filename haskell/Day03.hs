4792ef0bf86140cf363e820fade70d7fb8a70f2a
Cyrill Vetter <cyrill.vetter@gmail.com>
2023-11-22 15:52:33 +0100

    minor improvement

--- haskell/Day03.hs
+++ haskell/Day03.hs
@@ -1,13 +1,13 @@
 import Data.List (sort, intersect, elemIndex)
 import Data.List.Split (chunksOf)
-import Data.Maybe
+import Data.Maybe (fromJust)
 
 alph = ['a'..'z'] ++ ['A'..'Z']
 
 main = do
     input <- readFile "inputs/3.txt"
-    print . sum . map (getPriority . headIntersect . splitHalf) $ lines input
-    print . sum . map (getPriority . headIntersect) . chunksOf 3 $ lines input
+    print $ sum $ map (getPriority . headIntersect . splitHalf) $ lines input
+    print $ sum $ map (getPriority . headIntersect) $ chunksOf 3 $ lines input
 
 getPriority :: Char -> Int
 getPriority c = 1 + fromJust (c `elemIndex` alph)
